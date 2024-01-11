d8d3d67af3b3754ea797a8f7943f0ac212cd191f
Karolina Surma <ksurma@redhat.com>
2024-01-11 17:40:15 +0100

    And really strings

--- demo.c
+++ demo.c
@@ -3,7 +3,7 @@
 int main() {
     printf("Hello world!\n");
     printf("one plus one is %d\n", 1+1);
-    printf("one plus one is %d\n", "dva");
+    printf("one plus one is %s\n", "dva");
     return 0;
 }
 
