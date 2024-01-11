8125c5d8978084dc22d963d7fb25019040cfee10
Petr Viktorin <encukou@gmail.com>
2024-01-11 18:05:15 +0100

    Explore sizeof

--- demo.c
+++ demo.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
-    printf("jedna a jedna je %d\n", 1 + 1);
-    printf("jedna a jedna je %s\n", "dva");
+    int a = 1;
+    printf("Hodnota promenne je %d\n", a);
+    a = 2;
+    printf("Hodnota promenne je %d\n", a);
+
+    printf("Velikost int je %zd\n", sizeof(int));
+    printf("Velikost a je %zd\n", sizeof(a));
+    printf("Velikost retezce je %zd\n", sizeof(".."));
+    printf("Velikost long int je %zd\n", sizeof(long int));
+    printf("Velikost short int je %zd\n", sizeof(short int));
+    printf("Velikost char je %zd\n", sizeof(char));
+    printf("Velikost sizeof je %zd\n", sizeof(sizeof(char)));
+    printf("Velikost size_t je %zd\n", sizeof(size_t));
+
+    char c = 50;
+    printf("c=%d\n", c);
+    c += 5000;
+    printf("c=%d\n", c);
     return 0;
 }
