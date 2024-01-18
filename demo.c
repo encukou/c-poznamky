77c3b84916ee45e455bfb4022d18790a0827e3ae
Karolina Surma <ksurma@redhat.com>
2024-01-18 18:36:30 +0100

    Conditions, loops

--- demo.c
+++ demo.c
@@ -1,24 +1,37 @@
-// gcc -Wall -Werror --std=c17 demo.c -o demo
-// show me all warnings, treat warnings as errors, 
-// check whether the standard is followed (c17)
-//  -pedantic -- 
-
-/* multiline
-comment goes
-like this */
-
 #include <stdio.h>
 
-//  global variables when initialized always have a value 0
-int a;
 
 int main() {
-    printf("variable value is %d\n", a);
-    int b = 0;
-    printf("variable value is %d\n", b);
-    b = 2;
-    printf("variable value is %d\n", b);
-    const int c = 0;
+    {
+        int cislo = 1;
+        while (cislo <= 50) {
+            printf("%d\n", cislo);
+            // increment 1
+            cislo++;
+            // alternatively: cislo += 1
+        if (cislo > 5) {
+            break;
+        }
+        }
+    }
+
+    {
+        int cislo = 1;
+        do {
+            printf("%d\n", cislo);
+            cislo++;
+        } while (cislo <= 5);
+    }
+
+    {
+        int cislo = 1;
+        for (int cislo = 1; cislo <= 5; cislo++) {
+            if (cislo == 3) {
+                continue;
+            }
+            printf("%d\n", cislo);
+        }
+    }
 
     return 0;
 }
