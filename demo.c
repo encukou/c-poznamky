df8852e697b345750de442ee1e33835257298607
Karolina Surma <ksurma@redhat.com>
2024-01-18 17:34:01 +0100

    Comments, constants, cli args

--- demo.c
+++ demo.c
@@ -1,15 +1,24 @@
+// gcc -Wall -Werror --std=c17 demo.c -o demo
+// show me all warnings, treat warnings as errors, 
+// check whether the standard is followed (c17)
+//  -pedantic -- 
+
+/* multiline
+comment goes
+like this */
+
 #include <stdio.h>
 
-int main() {
-    long int a = 1;
-    printf("variable value is %d\n", a);
-    a = (long int)"dva";
-    printf("variable value is %d\n", a);
-    printf("Size of int is %zd\n", sizeof(int));
+//  global variables when initialized always have a value 0
+int a;
 
-    char c = 50;
-    c+= 5000;
-    printf("c=%d\n", c);
+int main() {
+    printf("variable value is %d\n", a);
+    int b = 0;
+    printf("variable value is %d\n", b);
+    b = 2;
+    printf("variable value is %d\n", b);
+    const int c = 0;
 
     return 0;
 }
