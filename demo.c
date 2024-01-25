e725d88c070c42dc87289f50321742d0b9576236
Petr Viktorin <encukou@gmail.com>
2024-01-25 16:54:29 +0100

    Loops

--- demo.c
+++ demo.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
 
-int main() {
-    int a = 1;
-    printf("Hodnota promenne je %d\n", a);
-    a = 2;
-    printf("Hodnota promenne je %d\n", a);
+int a = 0;
 
+int main() {
+    {
+        int a = 0;
+        //const int b = 0;
+        printf("Hodnota lok. promenne je %d\n", a);
+        a = 2;
+        printf("Hodnota lok. promenne je %d\n", a);
+    }
+    printf("Hodnota glob. promenne je %d\n", a);
+
+    {
+        int cislo = 1;
+        while (cislo <= 5) {
+            printf("%d\n", cislo);
+            cislo++;
+        }
+    }
+
+    //   inicializace   podminka   aktualizace
+    for (int cislo = 0; cislo < 5; cislo++) {
+        printf("%d\n", cislo);
+    }
+
+    {
+        int cislo = 1;
+        do {
+            printf("%d\n", cislo);
+            cislo++;
+        } while (cislo <= 5);
+    }
+
+    char c = 0;
+    while (c >= 0) {
+        c++;
+        printf("%d\n", c);
+    }
+
+/*
     printf("Velikost int je %zd\n", sizeof(int));
     printf("Velikost a je %zd\n", sizeof(a));
     printf("Velikost retezce je %zd\n", sizeof(".."));
@@ -19,5 +53,7 @@ int main() {
     printf("c=%d\n", c);
     c += 5000;
     printf("c=%d\n", c);
+*/
+
     return 0;
 }
