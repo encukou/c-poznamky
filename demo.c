8646cadf886de1e8246a7354ee97802d38530c4b
Petr Viktorin <encukou@gmail.com>
2024-02-01 18:03:50 +0100

    float, bool, switch

--- demo.c
+++ demo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 int main() {
     printf("Velikost int je %zd\n", sizeof(int));
@@ -16,55 +17,66 @@ int main() {
     printf("Velikost uint32_t je %zd\n", sizeof(uint32_t));
     // Víc na https://en.cppreference.com/w/c/types/integer
 
-    int a = 2147483647;
-    printf("max.hodnota int je %d\n", a);
-    a = -2147483648;
-    printf("min.hodnota int je %d\n", a);
-    printf("min.hodnota int je %d\n", INT_MIN);
+    printf("Velikost float je %zd\n", sizeof(float));
+    printf("Velikost double je %zd\n", sizeof(double));
 
-    unsigned u = 4294967295;
-    printf("max.hodnota unsigned int je %u\n", u);
-    u = 0;
-    printf("min.hodnota unsigned int je %u\n", u);
+    printf("Velikost bool je %zd\n", sizeof(bool));
 
-    int x = 0b0011;
-    int y = 0b1010;
-    printf("    x = %5d\n", x);
-    printf("    y = %5d\n", y);
+    bool b = true;
+    printf("b = %d\n", b);
+    b = false;
+    printf("b = %d\n", b);
+    b = (3 == 5) || (3 == 3);
+    printf("b = %d\n", b);
+    b = (3 == 5) && (3 == 3);
+    printf("b = %d\n", b);
+    b = !(3 == 3);
+    printf("b = %d\n", b);
 
-    printf("    x = %32b\n", x);
-    printf("    y = %32b\n", y);
+    b = 8 && 3;
+    printf("b = %d\n", b);
 
-    printf("x & y = %32b\n", x & y);
-    printf("x | y = %32b\n", x | y);
-    printf("   ~x = %32b\n", ~x);
-    printf("x ^ y = %32b\n", x ^ y);
+    b = 8 & 3;
+    printf("b = %d\n", b);
 
-    printf("x << 3= %32b\n", x << 3);
-    printf("y >> 2= %32b\n", y >> 2);
+    int i = -8;
+    if (i) {
+        printf("jo\n");
+    } else {
+        printf("ne\n");
+    }
+    b = i;
+    printf("b = %d\n", b);
 
-    uint32_t barva = 0xe51f40FF;
-    printf("barva = %x\n", barva);
-    printf("barva = %X\n", barva);
-    unsigned char cervena = (barva & 0xff000000) >> (8 * 3);
-    printf("cervena slozka = %x\n", cervena);
-    const uint32_t ZELENA_MASKA = 0x00ff0000;
-    unsigned char zelena = (barva & ZELENA_MASKA) >> (8 * 2);
-    printf("zelena slozka  = %x\n", zelena);
-    unsigned char modra = (barva & 0x0000ff00) >> (8 * 1);
-    printf("modra slozka   = %x\n", modra);
-    unsigned char pruhlednost = (barva & 0x000000ff) >> (8 * 0);
-    printf("pruhlednost    = %x\n", pruhlednost);
+    printf("!!i = %d\n", !!i);
 
-    uint32_t nova_barva = cervena << (8 * 3)
-                        | zelena << (8 * 2)
-                        | modra << (8 * 1)
-                        | pruhlednost;
-    printf("nova barva     = %x\n", nova_barva);
+    printf("i je: %s\n", i ? "pravdivé" : "nepravdivé");
 
-    printf("barva bez zelene = %x\n", barva & 0xff00ffff);
-    printf("barva bez zelene = %x\n", barva & ~ZELENA_MASKA);
-    printf("barva bez zelene = %x\n", barva &~ ZELENA_MASKA);
+    printf("i je: %u\n", (unsigned int)i );
+
+    double f = 3.1415926;
+    int j = f;
+    printf("f je: %d\n", (int)f );
+    printf("f je: %d\n", (bool)f );
+
+    f = 1 / (double)3;
+    printf("f je: %f\n", f );
+
+    i = 1;
+    switch (i) {
+        case 0:
+            printf("Je to nula\n");
+            break;
+        case 1:
+            printf("Je to jedna\n");
+            break;
+        case 2:
+            printf("Je to dva\n");
+            break;
+        default:
+            printf("Je to kdo vi co\n");
+            break;
+    }
 
     return 0;
 }
