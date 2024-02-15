91e2fb09c5902091983a3e77a3c71cdedd6f4508
Karolina Surma <ksurma@redhat.com>
2024-02-15 17:33:49 +0100

    memory and local/global variables

--- demo.c
+++ demo.c
@@ -3,50 +3,35 @@
 #include <math.h> 
 #include "demo.h"   // lokalni soubory v " ", systemove v <>
 
-// const int CENA = 25;
-#define CENA 25
-#define CENA 25 + 5  // to je spatne, bere se doslova
-#define CENA (25 + 5) // delat spis toto
-#define MOCNINA(x) x*x*x
+int g = 3;
 
+int recurse(int level) {
+    int i = 6;
+    printf("level %d - adresa %p\n", level, &i);
+
+    if (level > 0) {
+        recurse(level -1);
+    }
+}
 
 int main() {
-    printf("%d\n", treti_mocnina(5+1));
-    printf("%d\n", absolutni_hodnota(5+1));
-    printf("%d\n", absolutni_hodnota(-5+1));
-    int i = 7;
-    printf("%d\n", zvetsi(i));
+    int i = 6;
+    int j = 6;
+    int k = 6;
+    int l = 6;
+
     printf("%d\n", i);
-    vypis_cislo(i);
 
-    printf("%f\n", sin(1));
+    printf("%p\n", &i);
+    printf("%p\n", &j);
+    printf("%p\n", &k);
+    printf("%p\n", &l);
 
-#ifdef POUZIJ_MAKRO
-    printf("mocnina je %d\n", MOCNINA(3));
-#else
-    printf("mocnina je %d\n", 4*4*4);
-#endif
+    printf("%p\n", &g);
 
+    recurse(5);
+    printf("\n");
+    recurse(5);
 
     return 0;
-}
-
-int treti_mocnina(int cislo) {
-    return cislo * cislo * cislo;
-}
-
-int absolutni_hodnota(int cislo) {
-    if (cislo < 0) {
-        return -cislo;
-    }
-    return cislo;
-}
-
-int zvetsi(int cislo) {
-    cislo++;
-    return cislo;
-}
-
-void vypis_cislo(int cislo) {
-    printf("%d\n", cislo);
 }
