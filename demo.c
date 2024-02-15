e1d1e6f5d4d3d145dbd26df25a3e06057b3ab297
Karolina Surma <ksurma@redhat.com>
2024-02-15 17:35:59 +0100

    pointers and memory addresses

--- demo.c
+++ demo.c
@@ -5,33 +5,46 @@
 
 int g = 3;
 
-int recurse(int level) {
-    int i = 6;
-    printf("level %d - adresa %p\n", level, &i);
+void pricti_jedna(int *p) {
+    *p = *p + 1;
+    printf("nove x je %d\n", *p);
+}
 
-    if (level > 0) {
-        recurse(level -1);
-    }
+void podil_a_zbytek(int a, int b, int *adresa_podilu, int *adresa_zbytku) {
+    *adresa_podilu = a/b;
+    *adresa_zbytku = a%b;
 }
 
 int main() {
     int i = 6;
-    int j = 6;
-    int k = 6;
-    int l = 6;
-
     printf("%d\n", i);
 
-    printf("%p\n", &i);
-    printf("%p\n", &j);
-    printf("%p\n", &k);
-    printf("%p\n", &l);
+    // int * - misto v pameti, kde je ulozene cele cislo
+    int *ptr = &i;
 
-    printf("%p\n", &g);
+    // void * - misto v pameti, o kterem nic nevim
+    // ma to smysl jen jako vypsani
+    printf("%p\n", (void *)ptr);
 
-    recurse(5);
-    printf("\n");
-    recurse(5);
+    printf("%p\n", (void *)&g);
+
+    // dereference - jaka je hodnota, na kterou ukazuje ten ukazatel
+    // *ptr
+    int vysledek = *ptr + 1;
+    printf("%d\n", vysledek);
+
+    *ptr = 8;
+    printf("%d\n", i);
+    pricti_jedna(&i);
+    printf("%d\n", i);
+
+    int podil;
+    int zbytek;
+    podil_a_zbytek(70, 3, &podil, &zbytek);
+    printf("podil je %d\n", podil);
+    printf("zbytek je %d\n", zbytek);
+
+    int **pp = &ptr;
 
     return 0;
 }
