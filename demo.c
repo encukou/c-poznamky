08f84598a616e9b248da316f7f04b4baf172570e
Karolina Surma <ksurma@redhat.com>
2024-03-21 17:09:28 +0100

    hmmm

--- demo.c
+++ demo.c
@@ -1,52 +1,30 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <math.h> 
 #include "demo.h"   // lokalni soubory v " ", systemove v <>
 
-#define VELIKOST 6
-// nebo:
-// const int VELIKOST = 6;
+char *nazev_cisla(int i) {
+    switch (i) {
+        case 0: return "Nula";
+        case 1: return "Jedna";
+        default: return "Nevim";
+    }
+}
+void leak() {
+    int *p = malloc(sizeof(int));
 
-#define VELIKOST_POLE(P) (sizeof(P) / sizeof((P)[0]))
+    *p = 8;
+    printf("%d\n", *p);
+
+    free(p);
+}
+
 
 int main() {
+    leak();
 
-    int pole[VELIKOST] = {2, 3, 4, 5, 6, 7, -1};
-
-    // const int velikost = VELIKOST_POLE(pole);
-    // printf("%d\n", pole[0]);
-    // printf("%ld\n", sizeof(pole));
-    // printf("%ld\n", sizeof(pole[0]));
-
-    // for (int i = 0; i <velikost; i++) {
-    //     printf("%d\n", pole[i]);
-    // }
-
-    // // int *ptr = &pole[0];  // ptr ukazuje na prvni prvek pole
-    // int *ptr = pole;  // ptr ukazuje na prvni prvek pole
-
-
-    // for (int i = 0; i <velikost; i++) {
-    //     printf("prvek %i je %d\n", i, *(ptr+i));
-    // }
-
-    // for (int i = 0; i <velikost; i++) {
-    //     printf("prvek %i je %d\n", i, ptr[i]);
-    // }
-
-    // printf("%p ukazuje na %d\n", (void*)ptr, *ptr);
-
-    // for (int *current = ptr; *current != -1; current++) {
-    //     printf("prvek je %d\n", *current);
-    // }
-
-    char r[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-
-    // char *r = "Hello";
-
-    for (char *current = r; *current; current++) {
-        printf("prvek je %c\n", *current);
-    }
-
+    char *nazev = nazev_cisla(1);
+    printf("%s\n", nazev);
     return 0;
 }
