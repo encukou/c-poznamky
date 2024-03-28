14705b32e621ccab3ae08892cb049fa71a08a176
Petr Viktorin <encukou@gmail.com>
2024-03-28 18:00:55 +0100

    struct

--- demo.c
+++ demo.c
@@ -1,84 +1,38 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdint.h>
 
-char *viceradkovy_literal = "abcd\n"
-"efgh"  "\n"  "ij\nkl";
+typedef struct {
+    int x;
+    int y;
+} Souradnice;
 
-// vraci staticky retezec, nevolat free()
-char *nazev_jednoducheho_cisla(int n) {
-    switch (n) {
-        case 0: return "nula";
-        case 1: return "jedna";
-        case 2: return "dva";
-        case 3: return "tri";
-        case 4: return "ctyri";
-        case 5: return "pet";
-        case 6: return "sest";
-        case 7: return "sedm";
-        case 8: return "osm";
-        case 9: return "devet";
-        case 10: return "deset";
-        case 11: return "jedenact";
-        case 12: return "dvanact";
-        case 13: return "trinact";
-        case 14: return "ctrnact";
-        case 15: return "patnact";
-        case 16: return "sestnact";
-        case 17: return "sedmnact";
-        case 18: return "osmnact";
-        case 19: return "devatenact";
-        case 20: return "dvacet";
-        case 30: return "tricet";
-        case 40: return "ctyricet";
-        case 50: return "padesat";
-        case 60: return "sedesat";
-        case 70: return "sedmdesat";
-        case 80: return "osmdesat";
-        case 90: return "devadesat";
-        default: return "nevim dal";
-    }
-}
+typedef struct {
+    const char *jmeno;
+    int zivot;
+    int zraneni;
+    Souradnice poloha;
+} Prisera;
 
-size_t delka_retezce(char *retezec) {
-    // reimplementace strlen z <string.h>
-    size_t delka;
-    for (delka = 0; *retezec; delka++, retezec++) {
-        // empty
-    }
-    return delka;
-}
-
-// vraci dynamicky alokovanou pamet, kterou volajici musi uvolnit.
-char *jmeno_cisla(int cislo) {
-    int desitky = cislo / 10;
-    int jednotky = cislo % 10;
-    if (cislo <= 20 || jednotky == 0) {
-        char *nazev = nazev_jednoducheho_cisla(cislo);
-        char *buf = malloc(strlen(nazev) + 1);
-        strcpy(buf, nazev);
-        return buf;
-    }
-    char *jmeno_desitek = nazev_jednoducheho_cisla(desitky * 10);
-    size_t delka_desitek = strlen(jmeno_desitek);
-    char *jmeno_jednotek = nazev_jednoducheho_cisla(jednotky);
-    size_t delka_jednotek = strlen(jmeno_jednotek);
-
-    char *buf = malloc(delka_desitek + delka_jednotek + 1 + 1);
-    strcpy(buf, jmeno_desitek);
-    buf[delka_desitek] = ' ';
-    strcpy(buf + delka_desitek + 1, jmeno_jednotek);
-    return buf;
+void vypis_priseru(const Prisera *prisera) {
+    printf("prisera %s (%d/%d) na pozici %d,%d\n",
+           prisera->jmeno,  // to same jako:  (*prisera).jmeno
+           prisera->zivot - prisera->zraneni,
+           prisera->zivot,
+           prisera->poloha.x,
+           prisera->poloha.y);
+    //prisera->poloha_y += 1;
 }
 
 int main() {
-    for (int cislo = 0; cislo < 100; cislo++) {
-        char *jmeno = jmeno_cisla(cislo);
-        printf("%d %s\n", cislo, jmeno);
-        free(jmeno);
-    }
-    char znak = 97;
-    printf("%c %d\n", znak, znak);
-    printf("%s\n", viceradkovy_literal);
-    return 0;
+    Prisera karel = {
+        .jmeno = "Karel",
+        .zivot = 9,
+        .zraneni = 3,
+        .poloha = {1, 9},
+    };
+    karel.poloha.y += 1;
+    vypis_priseru(&karel);
+    vypis_priseru(&karel);
+    vypis_priseru(&karel);
+    vypis_priseru(&karel);
 }
