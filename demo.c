8be973db2f7fb16319048b71e817a9f141ef3906
Petr Viktorin <encukou@gmail.com>
2024-04-25 18:06:40 +0200

    Extension module

--- demo.c
+++ demo.c
@@ -1,49 +1,33 @@
-/* Spousteni:
+/*
 
-$ gcc -Wall -std=c17 -pedantic demo.c -Wimplicit-fallthrough -o demo $(python3-config --cflags --ldflags --embed)
-$ ./demo
+Překlad:
+
+gcc -Wall -std=c17 -pedantic demo.c -Wimplicit-fallthrough -o demo.so $(python3-config --cflags --ldflags) --shared -fPIC
+
+(bez --embed pro python3-config; s --shared pro gcc; výstupní soubor (-o) má příponu .so)
+
+
+python -c 'import demo; print(demo)'
 
 */
 
 #include <Python.h>
 
+PyModuleDef demo_def = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "demo",
+    .m_doc = NULL, // todo
+    .m_size = 0,
+    .m_methods = NULL,
+    .m_slots = NULL,
+};
 
-int main() {
-    int result = 1;
-    Py_Initialize();
-    PyObject *py_a = NULL;
-    PyObject *globals = PyDict_New();
-    if (globals == NULL) {
-        printf("Nastala chyba pri vytvareni slovniku!\n");
-        goto finally;
-    }
-    PyObject *vysledek = PyRun_String("a = 1.0 + 2\nprint(a)\ndel a", Py_file_input, globals, globals);
-    if (vysledek == NULL) {
-        printf("Nastala chyba pri provadeni programu!\n");
-        goto finally;
-    }
-    Py_CLEAR(vysledek);
-
-    py_a = PyMapping_GetItemString(globals, "a");
-    if (py_a == NULL) {
-        printf("Nastala chyba pri ziskavani promenne!\n");
-        goto finally;
-    }
-    long int c_a = PyLong_AsLong(py_a);
-    if (c_a == -1 && PyErr_Occurred()) {
-        printf("Nastala chyba pri prevodu na C long int!\n");
-        goto finally;
-    }
-
-    printf("1+2 = %ld\n", c_a);
-    result = 0;
-
-finally:
-    if (PyErr_Occurred()) {
-        PyErr_Print();
-    }
-    Py_CLEAR(py_a);
-    Py_CLEAR(globals);
-    Py_Finalize();
-    return result;
+PyMODINIT_FUNC
+PyInit_demo(void) {
+    /*
+    PyErr_SetString(PyExc_ValueError, "jeste neumim import");
+    return NULL;
+    */
+    return PyModuleDef_Init(&demo_def);
 }
+
