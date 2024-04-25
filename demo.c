6ff91e0fc7fe2d44c9ea681b75f48a5da509715e
Petr Viktorin <encukou@gmail.com>
2024-04-25 17:33:30 +0200

    Program s Pythonem!

--- demo.c
+++ demo.c
@@ -1,53 +1,49 @@
 /* Spousteni:
 
-$ gcc -Wall -std=c17 -pedantic demo.c -Wimplicit-fallthrough -o demo $(python3-config --cflags --libs --embed)
+$ gcc -Wall -std=c17 -pedantic demo.c -Wimplicit-fallthrough -o demo $(python3-config --cflags --ldflags --embed)
 $ ./demo
 
 */
 
 #include <Python.h>
 
+
 int main() {
+    int result = 1;
     Py_Initialize();
+    PyObject *py_a = NULL;
     PyObject *globals = PyDict_New();
     if (globals == NULL) {
         printf("Nastala chyba pri vytvareni slovniku!\n");
-        PyErr_Print();
-        Py_Finalize();
-        return 1;
+        goto finally;
     }
     PyObject *vysledek = PyRun_String("a = 1.0 + 2\nprint(a)\ndel a", Py_file_input, globals, globals);
     if (vysledek == NULL) {
         printf("Nastala chyba pri provadeni programu!\n");
-        PyErr_Print();
-        Py_DECREF(globals);
-        Py_Finalize();
-        return 1;
+        goto finally;
     }
-    Py_DECREF(vysledek);
+    Py_CLEAR(vysledek);
 
-    PyObject *py_a = PyMapping_GetItemString(globals, "a");
+    py_a = PyMapping_GetItemString(globals, "a");
     if (py_a == NULL) {
         printf("Nastala chyba pri ziskavani promenne!\n");
-        PyErr_Print();
-        Py_DECREF(globals);
-        Py_Finalize();
-        return 1;
+        goto finally;
     }
     long int c_a = PyLong_AsLong(py_a);
     if (c_a == -1 && PyErr_Occurred()) {
         printf("Nastala chyba pri prevodu na C long int!\n");
-        PyErr_Print();
-        Py_DECREF(py_a);
-        Py_DECREF(globals);
-        Py_Finalize();
-        return 1;
+        goto finally;
     }
 
     printf("1+2 = %ld\n", c_a);
+    result = 0;
 
-    Py_DECREF(py_a);
-    Py_DECREF(globals);
+finally:
+    if (PyErr_Occurred()) {
+        PyErr_Print();
+    }
+    Py_CLEAR(py_a);
+    Py_CLEAR(globals);
     Py_Finalize();
-    return 0;
+    return result;
 }
