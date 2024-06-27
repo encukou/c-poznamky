45e8b4ad927633b44a2bff8490bba1caf2f7b82c
Petr Viktorin <encukou@gmail.com>
2024-06-27 17:59:24 +0200

    D.ú.

--- demo.h
+++ demo.h
@@ -12,3 +12,26 @@ int llist_push(llist_type *list, llist_item_type item);
 int llist_pop(llist_type *list, llist_item_type *result);
 // on error, returns -1, sets *result to zero
 // on success, returns 0 and sets *result
+
+// d.ú.
+int llist_dump(llist_type *list);
+// print list contents using print, one number per line, either order
+// on error, returns -1
+// on success, returns 0
+
+// d.ú.
+ssize_t llist_count(llist_type *list);
+// return number of elements
+// on error, returns -1
+
+// d.ú.
+int llist_get(llist_type *list, ssize_t n, llist_item_type *result);
+// return n'th element
+// on error, returns -1, sets *result to zero
+// on success, returns 0 and sets *result
+
+// d.ú.
+int llist_remove(llist_type *list, ssize_t n, llist_item_type *result);
+// remove n'th element
+// on error, returns -1, sets *result to zero
+// on success, returns 0 and sets *result to removed item
