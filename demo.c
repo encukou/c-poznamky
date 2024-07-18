8e3b5e86e63bae141c9627ebb91c508ec6ebf6af
Karolina Surma <ksurma@redhat.com>
2024-07-18 17:05:23 +0200

    something about linked lists

--- demo.c
+++ demo.c
@@ -1,130 +1,22 @@
-/*
+#include "demo.h"
+#include <stdlib.h>
 
-Překlad:
-
-gcc -Wall -std=c17 -pedantic demo.c -Wimplicit-fallthrough -o demo.so $(python3-config --cflags --ldflags) --shared -fPIC
-
-(bez --embed pro python3-config; s --shared pro gcc; výstupní soubor (-o) má příponu .so)
-
-
-python -c 'import demo; print(demo); print(demo.get_none()); print(demo.add(1))'
-
-*/
-
-#include <Python.h>
-
-static PyObject *
-demo_get_none(PyObject *module, PyObject *unused)
-{
-    Py_RETURN_NONE;  // ekvivalentní k: `return Py_NewRef(Py_None);`
-    // ten kdo zavolal demo_get_none musí někdy zavolat Py_DECREF(Py_None);
-}
-
-// dú: `sum`, která sečte jakýkoli počet argumentů
-
-static PyObject *
-demo_add_c(PyObject *self, PyObject *const *args, Py_ssize_t nargs)
-{
-    if (nargs != 2) {
-        PyErr_Format(PyExc_TypeError, "demo.add expected 2 arguments (got %zd)", nargs);
-        return NULL;
-    }
-    long a = PyLong_AsLong(args[0]);
-    if (a == -1 && PyErr_Occurred()) {
-        return NULL;
-    }
-    long b = PyLong_AsLong(args[1]);
-    if (b == -1 && PyErr_Occurred()) {
-        return NULL;
-    }
-    long result = a + b;
-    return PyLong_FromLong(result);
-}
-
-static PyObject *
-demo_add_py(PyObject *self, PyObject *const *args, Py_ssize_t nargs)
-{
-    if (nargs != 3) {
-        PyErr_Format(PyExc_TypeError, "demo.add expected 3 arguments (got %zd)", nargs);
-        return NULL;
-    }
-    PyObject *intermediate = PyNumber_Add(args[0], args[1]);
-    if (intermediate == NULL) {
-        return NULL;
-    } // jinak mám na starost referenci `intermediate`
-    PyObject *result = PyNumber_Add(intermediate, args[2]);
-    Py_DECREF(intermediate);  // zahodím referenci
-    if (result == NULL) {
-        return NULL;
-    } // jinak mám na starost referenci `result`
-    return result;  // referenci pošlu dál
-}
-
-static PyObject *
-demo_sum(PyObject *self, PyObject *const *args, Py_ssize_t nargs)
-{
-    PyObject *zero = NULL;
-    PyObject *result = NULL;
-    PyObject *intermediate = NULL;
-
-    if (nargs == 0) {
-        result = PyLong_FromLong(0);
-        goto finally;
-    }
-
-    zero = PyLong_FromLong(0);
-    if (zero == NULL) {
-        goto finally;
-    }
-
-    intermediate = Py_NewRef(args[0]);
-    for (Py_ssize_t i = 1; i < nargs; i++) {
-        PyObject *call_args[] = {intermediate, args[i], zero};
-        PyObject *new = demo_add_py(self, call_args, 3);
-        Py_CLEAR(intermediate);
-        if (new == NULL) {
-            goto finally;
-        }
-        intermediate = new;
-    }
-    result = intermediate;
-    intermediate = NULL;
-finally:
-    Py_CLEAR(zero);
-    Py_CLEAR(intermediate);
-    return result;
-}
-
-
-static PyMethodDef demo_methods[] = {
-    {"get_none", demo_get_none, METH_NOARGS, "Return None"},
-    {"add_c", (PyCFunction)demo_add_c, METH_FASTCALL, "Add 2 numbers (C)"},
-    {"add_py", (PyCFunction)demo_add_py, METH_FASTCALL, "Add 3 numbers (Py)"},
-    {"sum", (PyCFunction)demo_sum, METH_FASTCALL, "Add any number of numbers (Py)"},
-    {0},
+struct llist_entry {
+    llist_item_type item;
+    struct llist_entry *prev;
 };
 
-PyDoc_STRVAR(
-    mod_doc,
-    "Modul kter\xc3\xbd um\xc3\xad skv\xc4\x9b"
-    "l\xc3\xa9 v\xc4\x9b" "ci!"
-);
-
-static PyModuleDef demo_def = {
-    .m_base = PyModuleDef_HEAD_INIT,
-    .m_name = "demo",
-    .m_doc = mod_doc,
-    .m_size = 0,
-    .m_methods = demo_methods,
-    .m_slots = NULL,
+struct llist_type {
+    struct llist_entry *head;
 };
 
-PyMODINIT_FUNC
-PyInit_demo(void)
+
+llist_type *llist_new(void) {
+    llist_type *list = malloc(sizeof(llist_type));
+    return list;
+}
+
+int llist_push(llist_type *list, list_item_type entry)
 {
-    /*
-    PyErr_SetString(PyExc_ValueError, "jeste neumim import");
-    return NULL;
-    */
-    return PyModuleDef_Init(&demo_def);
+    llist
 }
--- demo.h
+++ demo.h
@@ -1,4 +1,12 @@
-int treti_mocnina(int cislo);
-int absolutni_hodnota(int cislo);
-int zvetsi(int cislo);
-void vypis_cislo(int cislo);
+typedef int llist_item_type;
+
+typedef struct llist_type llist_type;
+
+llist_type *llist_new(void); // returns NULL on error
+void llist_free(llist_type *list);
+
+int llist_push(llist_type *list, llist_item_type entry);
+// returns -1 on error, 0 on success
+int llist_pop(llist_type *list, llist_item_type *result);
+// returns -1, 0 on error, sets *result to zero
+// returns 0 and sets *result
