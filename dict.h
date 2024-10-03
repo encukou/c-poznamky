802867094bd1e8792af9c5d23ecf368a6270f1bd
Petr Viktorin <encukou@gmail.com>
2024-10-03 17:58:14 +0200

    Add declarations for dict_iterator

--- dict.h
+++ dict.h
@@ -1,6 +1,7 @@
 /* A data type and functions for a mapping */
 
 typedef struct dict dict;
+typedef struct dict_iterator dict_iterator;
 
 // Parametrization of the dict
 typedef struct word * dict_key;
@@ -9,7 +10,10 @@ typedef int dict_value;
 
 
 // dict API
+
+// NULL on failure
 dict *dict_alloc(void);
+
 void dict_free(dict *);
 
 // 0 = success, -1 = fail
@@ -18,3 +22,13 @@ int dict_set(dict *d, dict_key key, dict_value value);
 
 // 1 = found, 0 = not found, -1 = fail
 int dict_get(dict *d, dict_key key, dict_value *result);
+
+
+// NULL on failure
+dict_iterator *dict_iterator_new(dict *d);
+
+void dict_iterator_free(dict_iterator *it);
+
+// 1 = found, 0 = at end (output set to NULL), -1 = fail
+// The dict owns the resulting key; it is valid until the next dict operation (except dict_get).
+int dict_iterator_next(dict_iterator *it, dict_key *key);
